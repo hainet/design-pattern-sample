3e901fcfc82e289b536c80cd954c7dc3a3085d3e
hainet <20400616+hainet@users.noreply.github.com>
2017-11-03 20:15:19 +0900

    Factory抽象クラスから不要なメソッドを除去

--- factory-method/src/com/hainet/framework/Factory.java
+++ factory-method/src/com/hainet/framework/Factory.java
@@ -3,9 +3,5 @@ package com.hainet.framework;
 // Factory Role
 public abstract class Factory {
 
-    public final Product Factory(String owner) {
-        return create(owner);
-    }
-
     public abstract Product create(String owner);
 }
